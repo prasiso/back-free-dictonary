59521082d07f521ac789c38ca0ee9a5284a7617e
Kelwin de Souza <kelwinsimoesdesouza@gmail.com>
2025-05-11 04:05:02 -0300

    feat: new end point list all entrie

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,16 @@
-import { timeout } from 'rxjs';
 import { PrismaService } from '../src/database/prisma.service';
 import fs from 'fs';
 const prisma = new PrismaService();
 
 const main = async () => {
   const data: any = await fs.readFileSync('mock/english.txt', 'utf-8');
-  const rows = data.split('\n');
+  const rows = data.split('\n')?.filter((dt) => dt);
   await prisma.$transaction(
     async (tx: PrismaService) => {
       const word = rows.map((entrie) => ({ entrie }));
       await tx.entries.createMany({
         data: word,
-        skipDuplicates: true
+        skipDuplicates: true,
       });
     },
     { timeout: 20000 },
