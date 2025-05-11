6e1e803805871057ded69509c6efbfe7a3ad9c74
Kelwin de Souza <kelwinsimoesdesouza@gmail.com>
2025-05-10 21:37:24 -0300

    feat: documentation in cadastro

--- src/modules/auth/decorator/sign_up/swagger/response.ts
+++ src/modules/auth/decorator/sign_up/swagger/response.ts
@@ -1,7 +1,7 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiExtraModels, ApiResponse } from '@nestjs/swagger';
+import { ApiResponse } from '@nestjs/swagger';
 import { ResponseGlobalSwagger } from 'src/decorator/response_global_swagger';
-import { Resp200Dto } from './dto-response';
+import { Resp200Dto, RespSignUp400Dto, RespSignUp409Dto } from './dto-response';
 export const ResponseSwagger = () => {
   return applyDecorators(
     ResponseGlobalSwagger(false),
@@ -11,36 +11,11 @@ export const ResponseSwagger = () => {
     }),
     ApiResponse({
       status: 400,
-      content: {
-        'application/json': {
-          examples: {
-            dto: {
-              summary: 'Dto Inválido',
-              value: {
-                timestamp: '2025-05-10T20:17:37.186Z',
-                path: '/',
-                status: 400,
-                message: [
-                  'Campo nome não pode ser vazio',
-                  'Campo Senha não pode ser vazio',
-                  'Campo email não pode ser vazio',
-                  'A senha deve ter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial.',
-                ],
-              },
-            },
-            usuarioExist: {
-              summary: 'Email encontrado',
-              value: {
-                timestamp: '2025-05-10T20:17:37.186Z',
-                path: '/',
-                status: 400,
-                message:
-                  'O e-mail informado já está cadastrado em nosso sistema. Por favor, utilize outro e-mail.',
-              },
-            },
-          },
-        },
-      },
+      type: RespSignUp400Dto
     }),
+    ApiResponse({
+      status: 409,
+      type: RespSignUp409Dto
+    })
   );
 };
