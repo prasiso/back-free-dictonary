3460f01ccda83e2576c1fff93818bd8dfa8d35e9
Kelwin de Souza <kelwinsimoesdesouza@gmail.com>
2025-05-12 09:21:13 -0300

    feat: end point history

--- src/services/history/history.service.ts
+++ src/services/history/history.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
-
+import { History } from './interface/history'
 @Injectable()
 export class HistoryService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
   async create({ id_entrie, id_user }) {
     return await this.prisma.history_read_entrie.create({
       data: {
@@ -11,5 +12,16 @@ export class HistoryService {
         id_user,
       },
     });
+  }
+
+  async find_all(
+    params: Prisma.history_read_entrieFindManyArgs,
+  ): Promise<{ count: number; rows: History[] }> {
+    const { where, ...rest } = params;
+    const [count, rows] = await Promise.all([
+      this.prisma.history_read_entrie.count({ where }),
+      this.prisma.history_read_entrie.findMany({ ...rest, where }),
+    ]);
+    return { count, rows };
   }
 }
