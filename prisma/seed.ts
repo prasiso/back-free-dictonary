fcd2aa86a561e144f991d859ed88b3c4465a9133
Kelwin de Souza <kelwinsimoesdesouza@gmail.com>
2025-05-15 00:59:57 -0300

    fix: seed time

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaService();
 
 const main = async () => {
   const data: any = await fs.readFileSync('mock/english.txt', 'utf-8');
-  const rows = data.split('\n')?.filter((dt) => dt);
+  const rows = data.split('\n')?.filter((dt) => dt).map(dt => dt.replaceAll('\r', '').trim());
   await prisma.$transaction(
     async (tx: PrismaService) => {
       const word = rows.map((entrie) => ({ entrie }));
@@ -13,7 +13,7 @@ const main = async () => {
         skipDuplicates: true,
       });
     },
-    { timeout: 300000000 },
+    { timeout: 60_000 },
   );
 };
 main();
